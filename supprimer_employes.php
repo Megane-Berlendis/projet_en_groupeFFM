e038e679b7121d733a82508ee713112596dd5485
Mégane BERLENDIS <berlendis_megane@gmx.fr>
2021-07-06 14:13:29 +0200

    début travail suppression employes

--- supprimer_employes.php
+++ supprimer_employes.php
@@ -1,2 +1,11 @@
 <?php require_once('header.php');?>
+
+<?php
+if(isset($_GET['action']) && $_GET['action'] == 'suppression'){
+    $pdo->query("DELETE FROM vehicule WHERE id_vehicule = '$_GET[id_vehicule]'");
+    header('Location: gestion_vehicule.php');
+}
+?>
+<form action=""></form>
+
 <?php require_once('footer.php');?>
